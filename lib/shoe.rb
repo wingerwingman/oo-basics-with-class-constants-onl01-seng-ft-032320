2b6e7136bb414bc1faf836ec989060046d4d1c94
Justin Winger <justinwinger4@gmail.com>
2020-03-30 17:10:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,11 +6,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-  end
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
-    BRANDS.each {|i| i !== brand ? BRANDS << brand}
+    if BRANDS.include?(brand)
+      BRANDS << brand 
+    end
   end
 
   def cobble
