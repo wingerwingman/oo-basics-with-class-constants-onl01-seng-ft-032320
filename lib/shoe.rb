9df2b62676cf82e9e34eac2f6d79ece8719dede9
Justin Winger <justinwinger4@gmail.com>
2020-03-30 16:56:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
-  BRAND = []
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -15,7 +15,7 @@ class Shoe
   
   def brand=(brand)
     @brand = brand
-    BRAND << brand 
+    BRANDS << brand 
   end
 
 end
